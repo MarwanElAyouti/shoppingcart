e56019b987f0f3cca5555a17e0bf32309d376b56
Marwan <marwan.hossam@gmail.com>
2023-08-07 19:41:18 +0200

    Be able to display the product prices in different currencies

--- shoppingcart/cart.py
+++ shoppingcart/cart.py
@@ -1,14 +1,16 @@
 import typing
 
-from . import product_prices
 from . import abc
+from .product_prices import DefaultProductPrices
+from .currency import Currency 
 from collections import OrderedDict
 
 class ShoppingCart(abc.ShoppingCart):
-    def __init__(self, product_prices: abc.ProductPriceDataSource = product_prices.DefaultProductPrices()):
+    def __init__(self, product_prices: abc.ProductPriceDataSource = DefaultProductPrices(), currency_code: str = "EUR"):
         self._items = OrderedDict()
         self._product_prices = product_prices
         self._total_price = 0.0
+        self._currency: Currency = Currency.get_currency(currency_code)
 
     def add_item(self, product_code: str, quantity: int):
         if product_code not in self._items:
@@ -28,13 +30,16 @@ class ShoppingCart(abc.ShoppingCart):
         for product_code, quantity in self._items.items():
             price = self._get_product_price(product_code) * quantity
 
-            price_string = "€%.2f" % price
+            price_string = self._format_price(price)
 
             lines.append(f"{product_code} - {quantity} - {price_string}")
-
-        lines.append(f"Total - €{self._total_price:.2f}")
+        total_price_string = self._format_price(self._total_price)
+        lines.append(f"Total - {total_price_string}")
         return lines
-
+    
     def _get_product_price(self, product_code: str) -> float:
         return self._product_prices.get_product_price(product_code)
     
+    def _format_price(self, amount: float) -> str:
+        converted_price = self._currency.convert_from_euro(amount)
+        return self._currency.format(converted_price)
---
+++ shoppingcart/currency.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+
+class CurrencySymbol(Enum):
+    USD = "$"
+    EUR = "€"
+    GBP = "£"
+    JPY = "¥"
+    # Add more currency codes and symbols as needed
+
+class Currency:
+    def __init__(self, code: str = "EUR", symbol: str = CurrencySymbol.EUR.value, conversion_rate_from_eur: float = 1.0):
+        self.code = code
+        self.symbol = symbol
+        self.conversion_rate_from_eur = conversion_rate_from_eur
+
+    def format(self, amount: float) -> str:
+        return f"{self.symbol}{amount:.2f}"
+
+    def convert_from_euro(self, amount: float) -> float:
+        if self.code == "EUR":
+            return amount
+        return amount * self.conversion_rate_from_eur 
+    
+
+    @classmethod
+    def get_currency(cls, code: str) -> 'Currency':
+        # Replace this implementation with your own mechanism to get the Currency object based on the code
+        # For example, you could use a currency mapping or fetch it from a database.
+        currencies = {
+            "USD": Currency("USD", CurrencySymbol.USD.value, 1.12),
+            "EUR": Currency("EUR", CurrencySymbol.EUR.value, 1.0),
+            "GBP": Currency("GBP", CurrencySymbol.GBP.value, 0.86),
+            "JPY": Currency("JPY", CurrencySymbol.JPY.value, 129.53),
+            # Add more currencies as needed
+        }
+        return currencies.get(code)
